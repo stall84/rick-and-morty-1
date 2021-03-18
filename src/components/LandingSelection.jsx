722ceb86c7baefff45ef7edceac12ddc077f02c4
Michael <stall84@gmail.com>
2021-03-18 14:53:13 -0400

    Initial commit with placeholder LandingSelection component and working Apollo client gql connection in Index.js

---
+++ src/components/LandingSelection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function LandingSelection(props) {
+    return (
+        <div>
+            <h2>LANDING SELECTION</h2>
+            <h4>Choose the character you want to know more about below</h4>
+        </div>
+    )
+}
+
+LandingSelection.propTypes = {
+
+}
+
+export default LandingSelection;
+
--- src/index.js
+++ src/index.js
@@ -4,6 +4,38 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/**
+ * @description Below importing and configuring Apollo's GraphQL client.
+ */
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache()
+});
+
+// Testing gql connection and tx/rx with hardcoded query below
+// REMEMBER TO REMOVE BEFORE PROD BUILD
+  client
+    .query({
+      query: gql`
+        query GetCharacters {
+          characters {
+            results {
+              id
+              name
+              gender
+              image
+            }
+          }
+        }
+      `
+    }).then((result) => console.log('GQL Query Result: ', result))
+      .catch((error) => console.log('The following error occurred: ', error))
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
